bfa69e7b86561bfcd87fcebc16538c789e61f9c6
krzshipon <krz.shipon1@gmail.com>
2024-11-27 14:02:38 +0600

    [SB][I][ems-backend][j] (Step 1 & 2) Project init & DB Connection
     - Initiating the Project form spring initializer
     - Connected mysql DB

---
+++ SpringBoot/intermediate/ems-backend/java/ems-backend-java/src/main/java/com/krz/ems_backend_java/EmsBackendJavaApplication.java
@@ -0,0 +1,13 @@
+package com.krz.ems_backend_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmsBackendJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmsBackendJavaApplication.class, args);
+	}
+
+}
---
+++ SpringBoot/intermediate/ems-backend/java/ems-backend-java/src/test/java/com/krz/ems_backend_java/EmsBackendJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krz.ems_backend_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmsBackendJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
