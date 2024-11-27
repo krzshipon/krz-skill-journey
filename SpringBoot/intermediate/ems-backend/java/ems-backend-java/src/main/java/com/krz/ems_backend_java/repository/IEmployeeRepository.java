01c79ebfdf52c019d74c9b13325c17be458834a0
krzshipon <krz.shipon1@gmail.com>
2024-11-27 16:18:12 +0600

    [SB][I][ems-backend][j] (Step 5-8) Employee Entity, Repo, Dto & mapper
     - Added Employee Entitiy
     - Added Employee repo
     - Added Employee DTO
     - Added Employee Mappers

---
+++ SpringBoot/intermediate/ems-backend/java/ems-backend-java/src/main/java/com/krz/ems_backend_java/repository/IEmployeeRepository.java
@@ -0,0 +1,7 @@
+package com.krz.ems_backend_java.repository;
+
+import com.krz.ems_backend_java.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IEmployeeRepository extends JpaRepository<Employee, Long> {
+}
